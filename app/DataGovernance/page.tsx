5ef281715ddcdd1de1998b9cd2d1ab0950e60d05
Kwanhooo <kwanhooo@foxmail.com>
2024-09-12 12:29:11 +0800

    fix: 数据总览和数据治理布局修改

--- app/DataGovernance/page.tsx
+++ app/DataGovernance/page.tsx
@@ -1,26 +1,50 @@
 import * as React from "react";
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
 
 const FourChartsPage = () => {
     return (
-
         <div className="h-full bg-black">
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 h-full">
-                <div className="bg-white rounded-2xl shadow-md flex flex-col">
-                    <div className="text-center text-xl font-bold py-1">原始数据</div>
-                    <div className="flex-grow bg-gray-200 rounded-b-2xl"></div>
-                </div>
-                <div className="bg-white rounded-2xl shadow-md flex flex-col">
-                    <div className="text-center text-xl font-bold py-1">预处理后数据</div>
-                    <div className="flex-grow bg-gray-200 rounded-b-2xl"></div>
-                </div>
-                <div className="bg-white rounded-2xl shadow-md flex flex-col">
-                    <div className="text-center text-xl font-bold py-1">平滑后数据</div>
-                    <div className="flex-grow bg-gray-200 rounded-b-2xl"></div>
-                </div>
-                <div className="bg-white rounded-2xl shadow-md flex flex-col">
-                    <div className="text-center text-xl font-bold py-1">多模态数据</div>
-                    <div className="flex-grow bg-gray-200 rounded-b-2xl"></div>
-                </div>
+                <Card className="flex flex-col">
+                    <CardHeader>
+                        <CardTitle className="text-xl font-bold">
+                            原始数据
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex-grow">
+                    </CardContent>
+                </Card>
+
+                <Card className="flex flex-col">
+                    <CardHeader>
+                        <CardTitle className="text-xl font-bold">
+                            预处理后数据
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex-grow">
+                    </CardContent>
+                </Card>
+
+                <Card className="flex flex-col">
+                    <CardHeader>
+                        <CardTitle className="text-xl font-bold">
+                            平滑后数据
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex-grow">
+                        <div className="flex-grow bg-gray-200 rounded-b-2xl"></div>
+                    </CardContent>
+                </Card>
+
+                <Card className="flex flex-col">
+                    <CardHeader>
+                        <CardTitle className="text-xl font-bold">
+                            多模态数据
+                        </CardTitle>
+                    </CardHeader>
+                    <CardContent className="flex-grow">
+                    </CardContent>
+                </Card>
             </div>
         </div>
     );
